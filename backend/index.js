33c47916863b6649e105ed815b3d9d8bccd9ca3b
남궁혁 <qorehdjd@naver.com>
2024-05-31 16:43:00 +0900

    2024-05-31(2)

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'http://www.count101.shop'],
     credentials: true,
     exposedHeaders: ['Autologin'],
   }),
